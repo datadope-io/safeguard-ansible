01a88083fe010f6db9b565d27f8dbc5e3b825470
Brad Nicholes <brad.nicholes@quest.com>
2023-01-18 10:05:03 -0700

    fix the setup.py and add version to the pipeline

--- credential_type_plugin/setup.py
+++ credential_type_plugin/setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=requirements,
     entry_points = {
         'awx.credential_plugins': [
-            'spp_plugin = spp_custom_credential_plugin:spp_plugin',
+            'spp_plugin = safeguardcredentialtype:spp_plugin',
         ]
     }
 )
